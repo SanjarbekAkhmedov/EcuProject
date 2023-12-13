04a9a91576f3169cbd74330aa74a8681efa660cf
Sanjar Akhmedov <sanjarakhmedov026@gmail.com>
2023-12-13 19:38:16 +0500

    changes

--- EcuProject/Pages/Create.cshtml.cs
+++ EcuProject/Pages/Create.cshtml.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace EcuProject.Pages.AppEcu
 {
     using EcuProject.Model;
+    using Microsoft.EntityFrameworkCore;
 
     public class CreateModel : PageModel
     {
@@ -20,13 +21,15 @@ namespace EcuProject.Pages.AppEcu
 
         public IActionResult OnGet()
         {
+            AppCarmakers = _context.AppCarmakers.ToList();
+            AppEcuTypes = _context.AppEcuTypes.ToList();
+            AppEcuProducers = _context.AppEcuProducers.ToList();
+
             return Page();
         }
 
         [BindProperty]
         public AppEcu AppEcu { get; set; } = default!;
-
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
@@ -39,5 +42,97 @@ namespace EcuProject.Pages.AppEcu
 
             return RedirectToPage("./Index");
         }
+
+        public IList<AppEcuType> AppEcuTypes { get; set; } = new List<AppEcuType>();
+
+        [BindProperty(SupportsGet = true)]
+        public int SelectedEcuTypeId { get; set; }
+
+        public IList<AppCarmaker> AppCarmakers { get; set; } = new List<AppCarmaker>();
+
+        [BindProperty(SupportsGet = true)]
+        public int SelectedCarmakerId { get; set; }
+
+        public IList<AppEcuProducer> AppEcuProducers { get; set; } = new List<AppEcuProducer>();
+
+        [BindProperty(SupportsGet = true)]
+        public int SelectedEcuProducerId { get; set; }
+
+        public List<BaseEntity> GetTable(string tableName)
+        {
+            tableName = GetEntityName(tableName);
+            if (string.IsNullOrEmpty(tableName))
+                return null;
+
+            var baseEntities = new List<BaseEntity>();
+
+            switch (tableName)
+            {
+                case nameof(AppEcuAcdelco):
+                    baseEntities.AddRange(_context.Set<AppEcuAcdelco>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuBosch):
+                    baseEntities.AddRange(_context.Set<AppEcuBosch>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuDelphi):
+                    baseEntities.AddRange(_context.Set<AppEcuDelphi>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuDenso):
+                    baseEntities.AddRange(_context.Set<AppEcuDenso>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuHitachi):
+                    baseEntities.AddRange(_context.Set<AppEcuHitachi>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuKefico):
+                    baseEntities.AddRange(_context.Set<AppEcuKefico>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuKeihin):
+                    baseEntities.AddRange(_context.Set<AppEcuKeihin>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuLuca):
+                    baseEntities.AddRange(_context.Set<AppEcuLuca>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuMagneti):
+                    baseEntities.AddRange(_context.Set<AppEcuMagneti>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuSagem):
+                    baseEntities.AddRange(_context.Set<AppEcuSagem>().Cast<BaseEntity>());
+                    break;
+                case "AppEcuSiemens":
+                    baseEntities.AddRange(_context.Set<AppEcuSiemen>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuTemic):
+                    baseEntities.AddRange(_context.Set<AppEcuTemic>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuTranstron):
+                    baseEntities.AddRange(_context.Set<AppEcuTranstron>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuTrw):
+                    baseEntities.AddRange(_context.Set<AppEcuTrw>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuValeo):
+                    baseEntities.AddRange(_context.Set<AppEcuValeo>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuVisteon):
+                    baseEntities.AddRange(_context.Set<AppEcuVisteon>().Cast<BaseEntity>());
+                    break;
+                default:
+                    // Handle the case when tableName doesn't match any known entity
+                    break;
+            }
+
+            return baseEntities;
+        }
+
+
+        public string GetEntityName(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return null;
+            string[] parts = text.Split('_');
+            string result = string.Join("", parts.Select(p => char.ToUpper(p[0]) + p.Substring(1)));
+            return result;
+        }
+
     }
 }
--- EcuProject/Pages/Edit.cshtml.cs
+++ EcuProject/Pages/Edit.cshtml.cs
@@ -36,6 +36,11 @@ namespace EcuProject.Pages.AppEcu
                 return NotFound();
             }
             AppEcu = appecu;
+
+            AppCarmakers = await _context.AppCarmakers.ToListAsync();
+            AppEcuTypes = await _context.AppEcuTypes.ToListAsync();
+            AppEcuProducers = await _context.AppEcuProducers.ToListAsync();
+
             return Page();
         }
 
@@ -70,6 +75,96 @@ namespace EcuProject.Pages.AppEcu
         private bool AppEcuExists(int id)
         {
             return _context.AppEcus.Any(e => e.Id == id);
+        }
+        public IList<AppEcuType> AppEcuTypes { get; set; } = new List<AppEcuType>();
+
+        [BindProperty(SupportsGet = true)]
+        public int SelectedEcuTypeId { get; set; }
+
+        public IList<AppCarmaker> AppCarmakers { get; set; } = new List<AppCarmaker>();
+
+        [BindProperty(SupportsGet = true)]
+        public int SelectedCarmakerId { get; set; }
+
+        public IList<AppEcuProducer> AppEcuProducers { get; set; } = new List<AppEcuProducer>();
+
+        [BindProperty(SupportsGet = true)]
+        public int SelectedEcuProducerId { get; set; }
+
+        public List<BaseEntity> GetTable(string tableName)
+        {
+            tableName = GetEntityName(tableName);
+            if (string.IsNullOrEmpty(tableName))
+                return null;
+
+            var baseEntities = new List<BaseEntity>();
+
+            switch (tableName)
+            {
+                case nameof(AppEcuAcdelco):
+                    baseEntities.AddRange(_context.Set<AppEcuAcdelco>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuBosch):
+                    baseEntities.AddRange(_context.Set<AppEcuBosch>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuDelphi):
+                    baseEntities.AddRange(_context.Set<AppEcuDelphi>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuDenso):
+                    baseEntities.AddRange(_context.Set<AppEcuDenso>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuHitachi):
+                    baseEntities.AddRange(_context.Set<AppEcuHitachi>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuKefico):
+                    baseEntities.AddRange(_context.Set<AppEcuKefico>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuKeihin):
+                    baseEntities.AddRange(_context.Set<AppEcuKeihin>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuLuca):
+                    baseEntities.AddRange(_context.Set<AppEcuLuca>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuMagneti):
+                    baseEntities.AddRange(_context.Set<AppEcuMagneti>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuSagem):
+                    baseEntities.AddRange(_context.Set<AppEcuSagem>().Cast<BaseEntity>());
+                    break;
+                case "AppEcuSiemens":
+                    baseEntities.AddRange(_context.Set<AppEcuSiemen>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuTemic):
+                    baseEntities.AddRange(_context.Set<AppEcuTemic>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuTranstron):
+                    baseEntities.AddRange(_context.Set<AppEcuTranstron>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuTrw):
+                    baseEntities.AddRange(_context.Set<AppEcuTrw>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuValeo):
+                    baseEntities.AddRange(_context.Set<AppEcuValeo>().Cast<BaseEntity>());
+                    break;
+                case nameof(AppEcuVisteon):
+                    baseEntities.AddRange(_context.Set<AppEcuVisteon>().Cast<BaseEntity>());
+                    break;
+                default:
+                    // Handle the case when tableName doesn't match any known entity
+                    break;
+            }
+
+            return baseEntities;
+        }
+
+
+        public string GetEntityName(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return null;
+            string[] parts = text.Split('_');
+            string result = string.Join("", parts.Select(p => char.ToUpper(p[0]) + p.Substring(1)));
+            return result;
         }
     }
 }
